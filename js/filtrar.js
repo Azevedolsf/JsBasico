166f026597faf931e51b0f1d823dde85d06e8cf4
Luiz Felipe de Azevedo Silva <luiz.azevedo@medbarra.com.br>
2018-06-29 14:41:00 -0300

    Atualizando estrutura RegExp

--- js/filtrar.js
+++ js/filtrar.js
@@ -1,26 +1,50 @@
 
-var campoInput = document.querySelector('#filtrarTabela');
+var filtrarCampo = document.querySelector("#filtrarTabela");
 
-campoInput.addEventListener('input', function(){
+filtrarCampo.addEventListener("input", function(){
     //console.log(this.value);
-    if(this.value.length > 0){
-        var pacientes = document.querySelectorAll(".paciente");
-         for(var i = 0; i < pacientes.length; i++){
-            var paciente = pacientes[i];
-            var tdNome = paciente.querySelector('.info-nome');
-            var nome = tdNome.textContent;
-            console.log(nome);
-            var expressoes = new RegExp(this.value, 'i');
-            if(!expressoes.test(nome)){
+    var pacientes = document.querySelectorAll('.paciente');
+    if(filtrarCampo.value.length > 0){
+        pacientes.forEach(function(paciente){
+            var nomeTd = paciente.querySelector(".info-nome");
+            var nome = nomeTd.textContent;     
+            var expressao = new RegExp(filtrarCampo.value, "i");
+            if(!expressao.test(nome)){
                 paciente.classList.add("invisivel");
             }else{
                 paciente.classList.remove("invisivel");
-            }
-        }
-    }else{
-        for(var i = 0; i < pacientes.length; i++){
-            var paciente = pacientes[i];
-            paciente.classList.remove("invisivel");
-        }
+            }      
+        })
+    }else{  //apenas quando o campo input estiver vazio
+        pacientes.forEach(function(paciente){
+            paciente.classList.remove("invisivel");    
+        })
     }
 })
+
+//Feito com o **FOR**
+// var filtrarCampo = document.querySelector("#filtrarTabela");
+
+// filtrarCampo.addEventListener("input", function(){
+//     //console.log(this.value);
+//     var pacientes = document.querySelectorAll('.paciente');
+//     if(filtrarCampo.value.length > 0){
+//         for(var i = 0; i < pacientes.length; i++){
+//             var paciente = pacientes[i];
+//             var nomeTd = paciente.querySelector(".info-nome");
+//             var nome = nomeTd.textContent;     
+//             var expressao = new RegExp(filtrarCampo.value, "i");
+//             if(!expressao.test(nome)){
+//                 paciente.classList.add("invisivel");
+//             }else{
+//                 paciente.classList.remove("invisivel");
+//             }      
+//         }
+//     }else{  //apenas quando o campo input estiver vazio
+//         for(var i = 0; i < pacientes.length; i++){
+//             var paciente = pacientes[i];
+//             paciente.classList.remove("invisivel");    
+//         }
+//     }
+// })
+
