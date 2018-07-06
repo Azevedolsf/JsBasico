cc11966d3c6aa6081f8d6347dfe36b5c0d523301
Luiz Felipe de Azevedo Silva <luiz.azevedo@medbarra.com.br>
2018-07-06 14:56:35 -0300

    final busca ajax

--- js/buscarPacientes.js
+++ js/buscarPacientes.js
@@ -4,17 +4,26 @@ buttonSearch.addEventListener("click", function() {
     console.log("Carregando...");
 
     var xhr = new XMLHttpRequest();
-    
+    //o open abre a connexão e configurar o método de requisição que deseja e pra onde quer fazer
     xhr.open("GET", "https://api-pacientes.herokuapp.com/pacientes");
-
+    //testa a mensagem de erro
+    //xhr.open("GET", "https://api-pacientes.herokuapp.com/paci11111entes");
+  
     xhr.addEventListener("load", function(){
-        //console.log(xhr.responseText);
-        var pacientes = JSON.parse(xhr.responseText);
-        console.log(pacientes);
-        pacientes.forEach(function(paciente){
-            pegaPacientesDaTabela(paciente);
-        });
+    var erroAjax = document.querySelector("#erro-ajax");
+        if(xhr.status == 200){
+            erroAjax.classList.add("invisivel");
+            //console.log(xhr.responseText);
+            var pacientes = JSON.parse(xhr.responseText);
+            console.log(pacientes);
+            pacientes.forEach(function(paciente){
+                pegaPacientesDaTabela(paciente);
+            });
+        }else{
+        console.log(xhr.status);
+        console.log(xhr.responseText);
+        erroAjax.classList.remove("invisivel");
+        } 
     });
-
-    xhr.send();
+    xhr.send();  
 });
--- js/removerPaciente.js
+++ js/removerPaciente.js
@@ -1,5 +1,3 @@
-var pacientes = document.querySelectorAll('.paciente');
-
 var tabela = document.querySelector("#tabela-pacientes");
 
 tabela.addEventListener("dblclick", function(event){
